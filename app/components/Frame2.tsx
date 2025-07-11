6e789781bdfea5fa8f27b4b6f3ba27253f0f3e31
Arshitha-20 <124654516+Arshitha-20@users.noreply.github.com>
2025-07-11 14:27:13 +0530

    Update Frame2.tsx

--- app/components/Frame2.tsx
+++ app/components/Frame2.tsx
@@ -1,60 +1,47 @@
+
 'use client';
 
-import { Box, Flex } from '@mantine/core';
-import JobCard from './JobCard';
+import { Job } from './FrameShowingJobs';
+import { id } from 'zod/v4/locales';
 
-export default function Frame2() {
-  return (
-    <Box
+export const  frame2Jobs: Job[] = [
+  {
+    id: '1',
+    logo: '/amazon.png',
+    title: 'Full Stack Developer',
+    experience: '1-3 yr Exp',
+    type: 'Onsite',
+    salary: '12LPA',
+    posted: '24h Ago'
+  },
+  {
+    id: '2',
+    logo: '/tesla.png',
+    title: 'Node Js Developer',
+    experience: '1-3 yr Exp',
+    type: 'Onsite',
+    salary: '12LPA',
+    posted: '24h Ago'
+  },
+  {
+    id:"3",
+    logo:'/swiggy.png',
+    title:'UX/UI Designer',
+    experience:'1-3 yr Exp',
+    type:'Onsite',
+    salary:'12LPA',
+    posted:'24h Ago'
+  },
+  {
+    id: '4',
+    logo: '/amazon.png',
+    title: 'Full Stack Developer',
+    experience: '1-3 yr Exp',
+    type: 'Onsite',
+    salary: '12LPA',
+    posted: '24h Ago'
+  }
 
-      style={{
-        height:'360px',
-        width:'1312',
-        margin: '20px auto',
-        top:'265px',
-        left:'64px',
-        gap:'16px'
-      }}
-    >
-      <Flex
-        gap="10px"
-        wrap="nowrap"
-        justify="center"
-        align="flex-start"
-      >
-        <JobCard
-          logo="/amazon.png"
-          title="Full Stack Developer"
-          experience="1-3 yr Exp"
-          type="Onsite"
-          salary="12LPA"
-          posted="24h Ago"
-        />
-        <JobCard
-          logo="/tesla.png" 
-          title="Node Js Developer"
-          experience="1-3 yr Exp"
-          type="Onsite"
-          salary="12LPA"
-          posted="24h Ago"
-        />
-        <JobCard
-          logo="/swiggy.png"
-          title="UX/UI Designer"
-          experience="1-3 yr Exp"
-          type="Onsite"
-          salary="12LPA"
-          posted="24h Ago"
-        />
-        <JobCard
-          logo="/amazon.png"
-          title="Full Stack Developer"
-          experience="1-3 yr Exp"
-          type="Onsite"
-          salary="12LPA"
-          posted="24h Ago"
-        />
-      </Flex>
-    </Box>
-  );
-}
+];
+  export default frame2Jobs;
+
